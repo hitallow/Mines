7d033bb612a567a0eb97109fefdc3bbf19e446d5
Hitallo William <hitallo91@gmail.com>
2019-05-14 20:21:05 -0300

    componente field

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,16 +1,30 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 
 import params from '../params'
 
 export default (props) => {
+    const { mined, opened, nearMines } = props
+
     const styleField = [styles.field]
+
+    if (opened) styleField.push(styles.opened)
 
     if (styleField.length === 1)
         styleField.push(styles.regular)
+    let color = null
+    if (nearMines > 0) {
+        if (nearMines == 1) color = '#2a228d7'
+        else if (nearMines == 2) color = '#2b520f'
+        else if (nearMines > 2 && nearMines < 6) color = '#f9060a'
+        else if (nearMines >= 6) color = '#f2211a9'
+    }
 
     return (
         <View style={styleField}>
+            {!mined && opened && nearMines > 0 ?
+                <Text style={[styles.label, { color: color }]}>{nearMines}
+                </Text> : false}
 
         </View>
     )
@@ -29,5 +43,16 @@ const styles = StyleSheet.create({
         borderTopColor: '#ccc',
         borderRightColor: '#333',
         borderBottomColor: '#333',
+    },
+    opened : {
+        backgroundColor: '#999',
+        borderColor : '#777',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    label: {
+        fontWeight : 'bold',
+        fontSize: params.fontSize,
+        
     }
 })
